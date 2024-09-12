05adb272cf8642f37175995da39540dba907b0a4
Umra-Badar20 <umrabadar555@gmail.com>
2024-09-12 10:36:41 +0500

    Quiz App done

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
 var questions = [
     {
-        question :"What is the correct syntax to declare a variable in JavaScript?",
+        question: "What is the correct syntax to declare a variable in JavaScript?",
         opt1: "var variableName;",
         opt2: "let variableName;",
         opt3: "const variableName;",
         opt4: "All of the above",
-        ans :  "All of the above"
+        ans: "All of the above"
     },
     {
-        question :"Which of the following is used to create an object in JavaScript?",
+        question: "Which of the following is used to create an object in JavaScript?",
         opt1: "{}",
         opt2: "[]",
         opt3: "()",
         opt4: "< >",
-        ans : "{}"
+        ans: "{}"
     },
     {
-        question :" Which method is used to parse a string to an integer in JavaScript?",
+        question: " Which method is used to parse a string to an integer in JavaScript?",
         opt1: "parseInt()",
         opt2: "parseInteger()",
         opt3: "parseNumber()",
         opt4: "parseFloat()",
-        ans : "parseInt()"
+        ans: "parseInt()"
     }
 ]
 var index = 0;
 var result = 0
 
-function renderQues(){
+function renderQues() {
     var container = document.getElementById("container");
     var option = document.getElementsByName("option");
-    for(var i =0; i<option.length; i++){
-        
-        if(option[i].checked){
-            if(questions[index-1].ans===option[i].value){
+    for (var i = 0; i < option.length; i++) {
+
+        if (option[i].checked) {
+            if (questions[index - 1].ans === option[i].value) {
                 result++
             }
-            console.log(option[i].value,  questions[index-1].ans, result);    
+            console.log(option[i].value, questions[index - 1].ans, result);
         }
     }
-   
-    if(!questions[index]){
-calculateResult()
-var resultContainer = document.getElementById("result-container");
-resultContainer.style.display = "block";
-container.style.display = "none";
+    if (!questions[index]) {
+        calculateResult()
+        var resultContainer = document.getElementById("result-container");
+        resultContainer.style.display = "block";
+        container.style.display = "none";
 
-        console.log("Result: "+ result);
+        console.log("Result: " + result);
         return;
     }
-   
-  
     container.innerHTML = `
-     <p class="question">${index+1})
+    <h3 class="text-center text-body-secondary">JavaScript Quiz</h3>
+
+     <p class="question">${index + 1}
      ${questions[index].question}</p><hr/>
         <div class="options"> <label for="opt1"><input type="radio" name="option" id="opt1" value="${questions[index].opt1}"> 
         ${questions[index].opt1}</label></div>
@@ -66,43 +65,33 @@ container.style.display = "none";
         <button id="next" class="m-2 btn btn-success" onclick="nextQuestion()">Next</button> `
     var prevButton = document.getElementById("prev");
     var nextButton = document.getElementById("next");
-    // var isChecked = false;
-    // for(var i = 0; i < option.length; i++){
-    //     if(option[i].checked){
-    //         isChecked = true;
-    //         break;
-    //     }
-    // }    
-    // nextButton.disabled = !isChecked;
-
 
     function calculateResult() {
         var score = document.getElementById("score");
-        var percentage = ((result/questions.length)*100).toFixed(2) 
-        
-        if(percentage>70){
-            score.innerHTML = "Congratulations! You passed the test.</br> You attempted " + result + " correct answers out of " + questions.length+ " questions. </br> Your score is " +percentage + "%";
-        }else{
-            score.innerHTML = "Sorry! You failed the test.</br> You attempted " + result + " correct answers out of " + questions.length+ " questions. </br> Your score is " +percentage + "%";
-            score.style.color="#dc3545";
+        var percentage = ((result / questions.length) * 100).toFixed(2)
+
+        if (percentage > 70) {
+            score.innerHTML = "Congratulations! You passed the test.</br> You attempted " + result + " correct answers out of " + questions.length + " questions. </br> Your score is " + percentage + "%";
+        } else {
+            score.innerHTML = "Sorry! You failed the test.</br> You attempted " + result + " correct answers out of " + questions.length + " questions. </br> Your score is " + percentage + "%";
+            score.style.color = "#dc3545";
         }
     }
-    prevButton.disabled = (index == 0 );
+    prevButton.disabled = (index == 0);
     if (index == questions.length - 1) {
         nextButton.innerHTML = "Submit";
-        // nextButton.style.backgroundColor="#dc3545";
         nextButton.classList.add("btn-danger")
         nextButton
-        console.log(questions.length); 
-      }  
+        console.log(questions.length);
+    }
 }
 renderQues()
-function nextQuestion(){
+function nextQuestion() {
     index++;
     renderQues();
 }
-function previousQuestion(){
-    if(index > 0){
+function previousQuestion() {
+    if (index > 0) {
         index--;
         renderQues();
     }
